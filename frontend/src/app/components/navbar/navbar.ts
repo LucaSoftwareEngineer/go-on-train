7f253d2b169ad65f859f962a43c44b205e38eb63
LucaSoftwareEngineer <lucapc40@gmail.com>
2026-03-27 01:48:34 +0100

    feat: implementa logout

--- frontend/src/app/components/navbar/navbar.ts
+++ frontend/src/app/components/navbar/navbar.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import SecureLS from 'secure-ls';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +10,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.scss',
 })
 export class Navbar {
+
+  ls = new SecureLS();
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
+
+  logoutHandler() {
+
+    this.ls.remove('token');
+    this.toastr.success('Logout effettuato...', 'Successo!')
+
+    setTimeout(() => {
+      this.router.navigate(['/']);
+    }, 2000)
+
+  }
 
 }
